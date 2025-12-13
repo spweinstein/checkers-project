1827d029b0ee3ace46a5b4097cd89eaf48733fea
Spencer <sweinstein34@gmail.com>
2025-12-12 20:47:33 -0500

    Started game logic

--- app.js
+++ app.js
@@ -2,27 +2,18 @@
 
 /*===========================CONSTANTS=======================*/
 
-const players = ["W", "B"];
+const players = ["White", "Black"];
 const pieceTypes = ["Regular", "King"];
 const boardValues = [];
 const boardCells = document.querySelectorAll("div.game > div.cell");
+const turn = "White";
+const isWinner = false;
+const isTie = false;
 
 /*===========================HELPER FUNCTIONS=======================*/
 
 function computeRowIndex(cellId) {
   return Math.floor(cellId / 8);
-}
-
-function addPiece(index, player, isKing = false) {
-  boardValues[index] = `player${player}`;
-}
-
-function hasPiece(index) {
-  return cellValues[index] !== "";
-}
-
-function removePiece(index) {
-  boardValues[index] = "";
 }
 
 /*===========================INITIALIZATION=======================*/
@@ -41,11 +32,9 @@ function initialize() {
     const isDark = (isRowEven && isCellEven) || (!isRowEven && !isCellEven);
 
     if (isPlayer1 && isPiece) {
-      addPiece(i, 0);
-      //   boardValues[i] = "W";
+      addPiece(i, players[0], "Regular");
     } else if (isPlayer2 && isPiece) {
-      //   boardValues[i] = "B";
-      addPiece(i, 1);
+      addPiece(i, players[1], "Regular");
     } else {
       removePiece(i);
     }
@@ -59,21 +48,85 @@ function initialize() {
 
 /*===========================RENDER=======================*/
 
-function updateBoard() {
+function render() {
   boardCells.forEach((cell, index) => {
     const cellValue = boardValues[index];
-    if (cellValue === "player0") {
+    if (cellValue.startsWith(players[0])) {
       cell.classList.add("has-piece", "white");
-    } else if (cellValue === "player1") {
+      if (cellValue.endsWith("king")) {
+        cell.classList.add("king");
+      }
+    } else if (cellValue.startsWith(players[1])) {
       cell.classList.add("has-piece", "black");
+      if (cellValue.endsWith("king")) {
+        cell.classList.add("king");
+      }
     } else {
-      cell.classList.remove("has-piece", "black", "white");
+      cell.classList.remove("has-piece", "black", "white", "king");
     }
   });
 }
 
-function render() {
-  updateBoard();
+/*===========================GAME LOGIC=======================*/
+function addPiece(index, player, pieceType) {
+  boardValues[index] = `${player}_${pieceType}`;
 }
 
+function hasPiece(index) {
+  return boardValues[index] !== "";
+}
+
+function removePiece(index) {
+  const pieceValue = boardValues[index];
+  boardValues[index] = "";
+  return pieceValue;
+}
+
+function crownPiece(index) {}
+
+function movePiece(fromIdx, toIdx) {
+  const [player, pieceType] = removePiece(fromIdx).split("_");
+  addPiece(toIdx, player, pieceType);
+}
+
+function hasDoubleJump(fromIdx) {}
+
+function getLegalMoves(index) {
+  const rowIndex = computeRowIndex(index);
+  const cell = boardCells[index];
+}
+
+function checkForWinner() {}
+
+function checkForTie() {}
+
+function switchPlayerTurn() {}
+
 initialize();
+
+/*===========================EVENT LISTENERS=======================*/
+
+function handleClick(event) {
+  const clickedElem = event.target;
+  const cellIndex = clickedElem.parentNode.id * 1;
+  const cell = boardCells[cellIndex];
+  const isPiece = hasPiece(cellIndex);
+  const cellValue = boardValues[cellIndex];
+  const isTurn = turn === cellValue.split("_")[0];
+  console.log(
+    `Clicked ${cellIndex}
+    Has piece: ${isPiece}
+    Cell value: ${cellValue}
+    Is player's turn: ${isTurn}
+    `
+  );
+
+  checkForWinner();
+  checkForTie();
+  switchPlayerTurn();
+  render();
+}
+
+document.querySelectorAll("#game div.cell > div").forEach((cell) => {
+  cell.addEventListener("click", handleClick);
+});
